585a0973791ad46809489907c93f2c2c9a988354
FilipMaksymiuk7 <filipex12045@wp.pl>
2023-03-01 16:51:33 +0100

    commit3

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 import sys
-
+5
 print(sys.version)
 
+cos = ["a", "b", "c"]
+a = 5
+g = input("liczba")
+g = int(g)
+while g > 0:
+    print(g)
+    for x in cos:
+        print(cos)
+    g -= 1
 
